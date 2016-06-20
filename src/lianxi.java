8c75565a17e33d533edb75f02da3833387f018ae
liuyutongQQ <932940344@qq.com>
2016-06-20 14:14:28 +0800

    jiggg

---
+++ src/lianxi.java
@@ -0,0 +1,8 @@
+/**
+ * Created by Administrator on 2016/6/20.
+ */
+public class lianxi {
+    public static void main(String[] args) {
+        System.out.println("hahahahahahahahahahah");
+    }
+}
